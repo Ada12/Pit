99397273ec8024d64e39f2bd567df273df300741
yangchen <ada_8463@163.com>
2015-05-15 23:43:43 +0800

    complete the first login page

--- gen/com/example/zeducation/R.java
+++ gen/com/example/zeducation/R.java
@@ -11,6 +11,7 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
+        public static final int blue_button=0x7f070001;
         public static final int my_color=0x7f070000;
     }
     public static final class dimen {
@@ -24,10 +25,18 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int background_login=0x7f020000;
+        public static final int background_login_div_bg=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
     }
     public static final class id {
-        public static final int action_settings=0x7f090000;
+        public static final int action_settings=0x7f090006;
+        public static final int login_div=0x7f090000;
+        public static final int login_password_input=0x7f090003;
+        public static final int login_user_input=0x7f090001;
+        public static final int password_edit=0x7f090004;
+        public static final int signin_button=0x7f090005;
+        public static final int username_edit=0x7f090002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -40,7 +49,14 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int hello=0x7f050004;
         public static final int hello_world=0x7f050002;
+        public static final int login_label_password=0x7f050006;
+        public static final int login_label_signin=0x7f050007;
+        public static final int login_label_username=0x7f050005;
+        public static final int login_register_link=0x7f05000a;
+        public static final int login_status_logging_in=0x7f050008;
+        public static final int login_username_hint=0x7f050009;
         public static final int title_activity_test=0x7f050003;
     }
     public static final class style {
@@ -70,5 +86,6 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int normalText=0x7f060002;
     }
 }
